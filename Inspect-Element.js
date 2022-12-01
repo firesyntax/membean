d3ef597b8298a9d037081d3aa0a68541f7da4376
xeitokyh <115661276+firesyntax@users.noreply.github.com>
2022-11-30 19:59:42 -0500

    Update Inspect-Element.js

--- Inspect-Element.js
+++ Inspect-Element.js
@@ -1,2 +1,1 @@
-//make this coding a bookmark and then click more, then the 2nd option [URL] edit and delete the already entered text, then enter the copied text below this and click done ONLY USE THIS IF IT IS BLOCKED WHEN YOU CTRL+SHIFT+I OR RIGHT CLICK AND YOU CAN'T CLICK ON INSPECT //
 javascript:(function () {     var script =  document.createElement('script');    script.src="//cdn.jsdelivr.net/npm/eruda";     document.body.appendChild(script);    script.onload = function () {         eruda.init()     } })();
